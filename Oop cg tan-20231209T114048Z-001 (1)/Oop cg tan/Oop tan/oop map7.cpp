b32e94ebad02eaa518dfec2c254ff1b1d36dbc75
TanishkaKadam <121933642+TanishkaKadam@users.noreply.github.com>
2023-12-11 21:23:41 +0530

    Create oop map7.cpp

---
+++ Oop cg tan-20231209T114048Z-001 (1)/Oop cg tan/Oop tan/oop map7.cpp
@@ -0,0 +1,36 @@
+
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+int main()
+{
+	typedef map<string,int> mapType; 
+	mapType populationMap;
+	
+	populationMap.insert(pair<string, int>("Maharashtra", 7026357));
+	populationMap.insert(pair<string, int>("Rajasthan", 6578936));
+	populationMap.insert(pair<string, int>("Karanataka", 6678993));
+	populationMap.insert(pair<string, int>("Punjab", 5789032));
+	populationMap.insert(pair<string, int>("West Bengal", 6676291));
+	populationMap.insert(pair<string, int>("Tamil Nadu", 8276291));
+	populationMap.insert(pair<string, int>("Delhi", 2234291));
+	populationMap.insert(pair<string, int>("Goa", 57661));
+	populationMap.insert(pair<string, int>("Kerala", 4476721));
+	populationMap.insert(pair<string, int>("Gujarat", 74643567));
+	
+	
+	
+	mapType::iterator iter; 
+	cout<<"========Population of states in India==========\n";
+	cout<<"\n Size of populationMap"<<populationMap.size()<<"\n";
+	string state_name;
+	cout<<"\n Enter name of the state :";
+	cin>>state_name;
+	iter = populationMap.find(state_name);
+	if( iter!= populationMap.end() )
+	cout<<state_name<<" 's population is "<<iter->second ;
+	else
+	cout<<"Key is not populationMap"<<"\n";
+	populationMap.clear();
+}
