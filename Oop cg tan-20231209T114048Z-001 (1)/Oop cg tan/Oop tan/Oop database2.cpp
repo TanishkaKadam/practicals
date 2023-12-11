6c1fbf2d8b057d1fcccd287a4606ef1113aa37bd
TanishkaKadam <121933642+TanishkaKadam@users.noreply.github.com>
2023-12-11 20:48:11 +0530

    Update Oop database2.cpp

--- Oop cg tan-20231209T114048Z-001 (1)/Oop cg tan/Oop tan/Oop database2.cpp
+++ Oop cg tan-20231209T114048Z-001 (1)/Oop cg tan/Oop tan/Oop database2.cpp
@@ -1,89 +1,67 @@
+//student database
+
 #include<iostream>
-#include<string.h>    // header file declares a set of functions to work strings.
+#include<string>    
 using namespace std;
-    
-        class db
-  {
-       int roll;
-       char name[20];
-       char Class[10];
-       char Div[10];
-       char dob[12];
-       char bg[5],city[10];
-       char phone[12],license[12];
 
-   public:
-          static int stdno;      // declaration of static variable
-          static void count()    // defination of static function
-     {
-          cout<<"\n No.of objects created: "<<stdno;
-     }
+class db {
+		int roll;
+		string name,Class,Div,dob,bg,city ,phone,license;
 
-         db()        // default constructor
-    {
-      roll=7;
-      strcpy(name,"Sachin");
-      strcpy(Class,"SE");
-      strcpy(Div,"A");
-      strcpy(dob,"13/08/1992");
-      strcpy(bg,"B+");
-      strcpy(city,"Pune");
-      strcpy(phone,"9123456789");
-      strcpy(license,"A1010");
-      ++stdno;
-    }
-                  void getdata()// defining member function
-         {
-          cout<<"\n\nEnter:name,roll,Class,Div,Dob,bg,city,phone,license \n\n";
-          cin>>name>>roll>>Class>>Div>>dob>>bg>>city>>phone>>license;
-         }
-                friend void display(db d);   // declaration of friend function
+	public:
+		static int stdno;      
+		static void count() {  
+			cout<<"\n No.of objects created: "<<stdno;    
+		}
 
-            ~db()   // destructor
-         {
-             cout<<"\n\n"<<this->name<<"(Object) is destroyed!\n";  
-         }
+		db() {      
+			roll=0;
+			name,Class,Div,dob,bg,city,phone,license="";
+			++stdno;
+		}
+		
+		void getdata() { 
+			cout<<"\n\nEnter:Name,Roll no,Class,Div,Dob,Blood group,City,Phone,License \n\n";
+			cin>>name>>roll>>Class>>Div>>dob>>bg>>city>>phone>>license;
+		}
+		
+		friend void display(db d);   
+		~db() { 
+			cout<<"\n\n"<<this->name<<"(Object) is destroyed!\n";
+		}
 };
+int db::stdno; 
+void display(db d) {  
+	cout<<"\n Name:"<<d.name;
+	cout<<"\n Roll_No:"<<d.roll;
+	cout<<"\n Class:"<<d.Class;
+	cout<<"\n Div:"<<d.Div;
+	cout<<"\n DOB:"<<d.dob;
+	cout<<"\n Blood group:"<<d.bg;
+	cout<<"\n City:"<<d.city;
+	cout<<"\n Phone_No:"<<d.phone;
+	cout<<" \n License_No:"<<d.license;
+}
 
-         void display(db d) // defination of friend function
-       {
-         cout<<"\n Name:"<<d.name;
-         cout<<"\n Roll_No:"<<d.roll;
-         cout<<"\n Class:"<<d.Class;
-         cout<<"\n Div:"<<d.Div;
-         cout<<"\n DOB:"<<d.dob;
-         cout<<"\n Blood group:"<<d.bg;
-         cout<<"\n City:"<<d.city;
-         cout<<"\n Phone_No:"<<d.phone;
-         cout<<" \n Liacense_No:"<<d.license; 
-      }
- int db::stdno; // Define static data member stdno outside the class;
 
-                 int main()
-      { 
-          int n,i;
-          db d1,*ptr[5];  
-          cout<<"\nDefault values:";
-          display(d1);
+int main() {
+	int n,i;
+	db *ptr[5];  
 
-             d1.getdata();
-             display(d1);
+	cout<<"\nHow many objects u want to create?:";
+	cin>>n;
+	for(i=0; i<n; i++) {
+		ptr[i]=new db(); 
+		ptr[i]->getdata(); 
+	}
 
-               cout<<"\nHow many objects u want to create?:";
-               cin>>n;
-            for(i=0;i<n;i++)
-           {                   
-              ptr[i]=new db(); //new operator use to dynamic memory(run time) allocation
-              ptr[i]->getdata(); 
-           }
-               cout<<"\n"<<"name"<<"roll"<<"Class"<<"Div"<<"dob"<<"bg"<<"contact"<<"phone"<<"license";
-                for(i=0;i<n;i++)
-                 display(*ptr[i]);
-                db::count();  // calling of static function
+	for(i=0; i<n; i++)
+		display(*ptr[i]);
+		
+	db::count();  
 
-             for(i=0;i<n;i++)
-             {
-                 delete(ptr[i]); //delete operator use to deallocation of memory
-             }
-            cout<<"\nObjects deleted!" ;
-      }
+	for(i=0; i<n; i++) {
+		delete(ptr[i]); 
+	}
+	
+}
